3bce7c55330b768a16d8fea0027c934832de09ce
Богдан Македонський <makedonskyi.b@donnu.edu.ua>
2020-10-20 23:31:04 +0300

    finished

--- Laba_5/Program.cs
+++ Laba_5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Laba_5
 {
@@ -7,6 +8,7 @@ namespace Laba_5
     {
         static void Main(string[] args)
         {
+            Console.InputEncoding = System.Text.Encoding.Unicode;
             Console.OutputEncoding = System.Text.Encoding.Unicode;
 
             List<int> x = new List<int>();
@@ -17,10 +19,34 @@ namespace Laba_5
             string type = Console.ReadLine();
             int length = 0;
             int length1 = 0;
-
             if (type == "1")
             {
+                char[] charSeparators = new char[] { ' ' };
+                string[] res;
+                //Витягування данних із файлів
+                FileStream file_x = new FileStream("Massive X.txt", FileMode.Open, FileAccess.Read);
+                
+                StreamReader x_read = new StreamReader(file_x);
+                string info_x = x_read.ReadLine();
+                res = info_x.Split(charSeparators, StringSplitOptions.None);
+                length = res.Length;
 
+                //Запис данних у масив х
+                foreach (string i in res)
+                    x.Add(Convert.ToInt32(i));
+                    
+
+
+                FileStream file_y = new FileStream("Massive Y.txt", FileMode.Open, FileAccess.Read);
+
+                StreamReader y_read = new StreamReader(file_y);
+                string info_y = y_read.ReadLine();
+                res = info_y.Split(charSeparators, StringSplitOptions.None);
+                length1 = res.Length;
+
+                //Запис данних у масив y
+                foreach (string i in res)
+                    y.Add(Convert.ToInt32(i));
             }
 
             else if (type == "2")
@@ -46,11 +72,6 @@ namespace Laba_5
 
                 } while (flag_x);
 
-                //Вивід масиву х
-                Console.WriteLine("Масив x ");
-                foreach (int i in x)
-                    Console.Write(i + ", ");
-
                 // Заповнення масиву у
                 bool flag_y = true;
                 Console.WriteLine("\n");
@@ -61,24 +82,28 @@ namespace Laba_5
                     temp = Console.ReadLine();
 
                     if (temp == " ")
-                    {
                         flag_y = false;
-                    }
                     else
                     {
-                        x.Add(Convert.ToInt32(temp));
+                        y.Add(Convert.ToInt32(temp));
                         length1++;
                     }
 
                 } while (flag_y);
-
-                //Вивід масиву y
-                Console.WriteLine("Масив y ");
-                foreach (int i in y)
-                    Console.Write(i + ", ");
-
             }
-            
+
+            //Вивід масиву х
+            Console.WriteLine("Масив x ");
+            foreach (int i in x)
+                Console.Write(i + ", ");
+
+            Console.WriteLine(" \n ");
+
+            //Вивід масиву y
+            Console.WriteLine("Масив y ");
+            foreach (int i in y)
+                Console.Write(i + ", ");
+
             Console.WriteLine(" \n ");
 
             Console.WriteLine("Завдання 1. Збільшити парні елементи масиву х вдвічі" + "\n");
@@ -103,10 +128,10 @@ namespace Laba_5
                 count = length1;
             else
                 count = length;
-
+            
             for (int i = 0; i < count; i += 1)
-                z.Insert(i, x[i] * y[i]);
-
+                z.Add(x[i] * y[i]);
+            
             Console.WriteLine("Масив z:");
             foreach (int i in z)
                 Console.Write(i + ", ");
